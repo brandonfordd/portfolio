3b74e6c721dca5d1c2d9310e361676608cce7c33
Brandon Ford <Brandon.ford.617@gmail.com>
2021-04-13 12:15:32 -0400

    adding package files for timestamps

---
+++ index.js
@@ -0,0 +1,35 @@
+const fetch = require("node-fetch");
+
+const reposArray = [
+  "readme_generator",
+  "portfolio",
+  "weather_app",
+  "day_planner",
+  "javascript_quiz",
+  "password_generator",
+  "horiseon_refactor",
+  "prework-about-me"
+];
+
+function setModifiedDate() {
+  const url = "https://api.github.com/users/brandonfordd";
+
+  const options = {
+    headers: {
+      Authorization: "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"
+    }
+  };
+
+
+  fetch(url, options)
+    .then( res => res.json() )
+    .then( data => console.log(data)
+
+    );
+}
+
+setModifiedDate();
+
+
+
+    
